b446b33cc0d855e04973ccc3f7567e244a0d580a
HSCorp666 <81492966+HSCorp666@users.noreply.github.com>
2021-06-13 16:22:19 -0400

    Update cv.py

--- cv.py
+++ cv.py
@@ -1,7 +1,7 @@
 from flask_opencv_streamer.streamer import Streamer
-from gpiozero import LED
 import cv2
 import time
+from gpiozero import LED
 
 port = 3030
 require_login = False
@@ -10,20 +10,22 @@ streamer = Streamer(port, require_login)
 faceCas = cv2.CascadeClassifier('face.xml')
 
 
+led = LED(18)
+
 cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
     frame = cv2.flip(frame, 0)
 
-    faces = faceCas.detectMultiScale(1.1, 4)
+    faces = faceCas.detectMultiScale(frame, 1.1, 4)
 
     for x, y, w, h in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        LED.on()
-        time.sleep(1.5)
-        LED.off()
-
+        led.on()
+        time.sleep(1)
+        led.off()
+        
     streamer.update_frame(frame)
 
     if not streamer.is_streaming:
